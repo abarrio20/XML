b361105bcccad81d2d9e43f16ad75a0381080c65
abarrio20 <abarrio20@jaumebalmes.net>
2021-04-27 17:31:33 +0200

    Document principal de l'aplicació

--- Projecte_BP/src/Stock.java
+++ Projecte_BP/src/Stock.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
-public class Principal {
+
+public class Stock {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        String opcio;
-        System.out.println("Benvinguts a la botiga online de Bon Preu ");
+        String aproducte;
+        String consulta;
         System.out.println("Si us plau, quina opció vol triar(stock/vendes/sortir)? ");
-        opcio = entrada.next();
+        aproducte = entrada.next();
     }
 }
+
