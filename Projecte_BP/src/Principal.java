af172048cf128cec5df52024fa114e58b5c46c6c
abarrio20 <abarrio20@jaumebalmes.net>
2021-04-27 17:23:01 +0200

    Document principal de l'aplicació

---
+++ Projecte_BP/src/Principal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Benvinguts a la botiga online de Bon Preu ");
+        System.out.println("Si us plau, quina opció vol triar(stock/vendes/sortir)? ");
+    }
+}
