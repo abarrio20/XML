fa330945b4ea488b5ce93a57ec6bd0f5f3fec699
mduarte20 <mduarte20@jaumebalmes.net>
2021-05-04 16:10:11 +0200

    ajekfadhfño

--- Projecte_BP/src/Principal.java
+++ Projecte_BP/src/Principal.java
@@ -2,9 +2,31 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
         String opcio;
+
         System.out.println("Benvinguts a la botiga online de Bon Preu ");
+        menuPrincipal(entrada);
+
+
+    }
+
+    private static void menuPrincipal(Scanner entrada) {
+        String opcio;
         System.out.println("Si us plau, quina opció vol triar(stock/vendes/sortir)? ");
         opcio = entrada.next();
+        switch(opcio){
+
+            case "stock":
+                Stock.controlstock();
+                break;
+            case  "vendes":
+                Vendes.controlVendes();
+                break;
+            case "sortir":
+
+                break;
+        }
     }
+
 }
