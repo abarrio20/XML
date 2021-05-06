d6d1b596ea27d76422911e8ed63d2fe2df2a3ebd
mduarte20 <mduarte20@jaumebalmes.net>
2021-05-06 10:53:03 +0200

    holaa

---
+++ Projecte_BP/src/AfegirProductes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AfegirProductes {
+    static void llegirStock(){
+        Scanner entrada = new Scanner (System.in);
+
+        String p = entrada.next();
+        System.out.println(p);
+
+        p = entrada.next();
+        System.out.println(p);
+
+        p = entrada.next();
+        System.out.println(p);
+
+    }
+}
